4ee701258df5235cef3878f19f07cc955c9a895a
Chris Herrmann <cherrmann@lemon-digital.com>
2022-05-25 14:35:56 +0200

    add head and base metas

--- src/components/layout/LayoutMain.tsx
+++ src/components/layout/LayoutMain.tsx
@@ -1,20 +1,47 @@
 import React, { useEffect, useState } from 'react';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
 
 import { Container, Row, Col } from 'reactstrap';
 
-
 export const LayoutMain = ({ children }: any) => {
 
-    return (
-        <Container>
-            <Row>
-                <Col>
-                    {children}
-                </Col>
-            </Row>
-        </Container>
-        
-    )
+	return (
+		<>
+			<Head>
+				<title>SEO and Speed test friendly boilerplates</title>
+				<meta name="title" content="SEO and Speed test friendly boilerplates" key="title_name" />
+				<meta
+					name="description"
+					content="Lightning fast, highly optimised pages Google will love"
+					key="description_name"
+				/>
+
+				<meta property="og:type" content="website" key="type_property" />
+				<meta property="og:site_name" content="Lemon Digital Design" key="name_property" />
+				<meta property="og:title" content="SEO and Speed test friendly boilerplates" key="title_property" />
+				<meta
+					property="og:description"
+					content="Lightning fast, highly optimised pages Google will love"
+					key="description_property"
+				/>
+				<meta property="og:keywords" content="SEO, Google, Speed test" key="keywords_property" />
+				<meta property="og:url" content="https://www.carsvansandbikes.com" key="url" />
+				<meta
+					property="og:image"
+					itemProp="image"
+					content="https://cdn.carsvansandbikes.com/image/upload/media/documents/SharingLogo-1200x630.png"
+					key="image"
+				/>
+				<meta name="application-name" content="lemon-digital.com" key="appName" />
+			</Head>
+
+			<Container>
+				<Row>
+					<Col>
+						{children}
+					</Col>
+				</Row>
+			</Container>
+		</>
+	)
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
 
 import styles from '../styles/pages/home/home.module.scss'
 
