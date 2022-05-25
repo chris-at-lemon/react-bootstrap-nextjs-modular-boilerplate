638e66a008c000e4aff9ab3669cf800fb89163b5
Chris Herrmann <cherrmann@lemon-digital.com>
2022-05-25 14:08:28 +0200

    add home copy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+
 import styles from '../styles/pages/home/home.module.scss'
 
 const Home: NextPage = () => {
   return (
-    <h1>React Bootstrap NextJS Modular SCSS Boilerplate</h1>
+      <div className={`${styles['landingHero']}`}>
+          <h1>React Bootstrap NextJS Modular SCSS Boilerplate</h1>
+          <h2>Lightning fast, highly optimised pages Google will love</h2>
+      </div>
   )
 }
 
