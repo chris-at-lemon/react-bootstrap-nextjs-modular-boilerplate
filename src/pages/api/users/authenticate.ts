0c660aa2aedceaf82ca6022a52093ec430faffa2
Chris Herrmann <cherrmann@lemon-digital.com>
2022-06-02 13:26:08 +0200

    add http methods

---
+++ src/pages/api/users/authenticate.ts
@@ -0,0 +1,41 @@
+import jwt from 'jsonwebtoken';
+import getConfig from 'next/config';
+import { setHttpAgentOptions } from 'next/dist/server/config';
+
+const { serverRuntimeConfig } = getConfig();
+
+console.log('authenticating');
+
+
+// users in JSON file for simplicity, store in a db for production applications
+import users from '../../../data/users.json';
+
+function authenticate(req: any, res: any) {
+
+	const { username, password } = req.body;
+	const user = users.find((u: { username: string; password: string; }) => u.username === username && u.password === password);
+
+	if (!user) {
+		return res.status(401).json({
+			message: 'Not authorised'
+		})
+	}	else {
+		const token = jwt.sign(
+			{
+				username: 'userChris',
+				firstName: 'Chris',
+				lastName: 'Test',
+			},
+			serverRuntimeConfig.secret,
+			{ expiresIn: '1h' });
+	
+		return res.status(200).json({
+			jwt: token
+		});
+	}
+	
+	// create a jwt token that is valid for 7 days
+
+}
+
+export default authenticate;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,20 @@
 import type { NextPage } from 'next'
+import { loginUser } from '../modules/auth'
 
 import styles from '../styles/pages/home/home.module.scss'
 
 const Home: NextPage = () => {
+
   return (
+    <>
       <div className={`${styles['landingHero']}`}>
           <h1>React Bootstrap NextJS Modular SCSS Boilerplate</h1>
           <h2>Lightning fast, highly optimised pages Google will love</h2>
+          <div>
+        <button onClick={() => loginUser({username: 'userChris', password: 'test'})}>Login</button>
       </div>
+      </div>
+    </>
   )
 }
 
