fa60985b0e8b196d12ff6f6ab96e3596db5c7d3b
Chris Herrmann <cherrmann@lemon-digital.com>
2022-07-13 12:50:47 +0200

    clean repo

--- src/controllers/pages/homeController.ts
+++ src/controllers/pages/homeController.ts
@@ -1,24 +1,11 @@
 import { useState } from 'react'
-import { loginUser, getUserLoginData, checkUser, signOut } from '../../modules/auth'
 
 export const HomeController = () => {
-    const [showLoginCard, setShowLoginCard] = useState<boolean>(false)
-    const toggleLoginCard = () => {
-        setShowLoginCard(!showLoginCard)
-    }
 
-    const handleLogin = async () => {
-        loginUser({username: 'userChris', password: 'test'})
-    }
 
     return {
-        showLoginCard,
         fn: {
-            handleLogin,
-            getUserLoginData,
-            checkUser,
-            signOut,
-            toggleLoginCard
+
         }
     }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,10 @@
 import type { NextPage } from 'next'
 
-import LoginCard from '../components/login/loginCard'
-
 import { HomeController } from '../controllers/pages/homeController'
 import styles from '../styles/pages/home/home.module.scss'
 
 const Home: NextPage = () => {
-  const { fn, showLoginCard } = HomeController();
+  const { fn } = HomeController();
 
   return (
     <>
@@ -16,18 +14,6 @@ const Home: NextPage = () => {
         <div className={`${styles['landingHero']}`}>
         <h1>React Bootstrap NextJS Modular SCSS Boilerplate</h1>
         <h2>Lightning fast, highly optimised pages Google will love</h2>
-        <div>
-          <div className={`${styles['loginBtnWrapper']}`}>
-            {!showLoginCard &&
-              <button className={`${styles['btn']} ${styles['btn-primary']}`} onClick={fn.toggleLoginCard}>Login</button>
-            }
-          </div>
-          <div className={`${styles['loginCardWrapper']}`}>
-            {showLoginCard &&
-              <LoginCard />
-            }
-          </div>
-        </div>
       </div>
         </div>
       </div>
