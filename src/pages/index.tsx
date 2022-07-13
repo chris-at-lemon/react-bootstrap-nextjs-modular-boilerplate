c34890f529f2d06f5ebd592883f0ee82834562e0
Chris Herrmann <cherrmann@lemon-digital.com>
2022-07-13 13:42:13 +0200

    cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import { HomeController } from '../controllers/pages/homeController'
 import styles from '../styles/pages/home/home.module.scss'
 
 const Home: NextPage = () => {
-  const { fn, showLoginCard } = HomeController();
+  const {  } = HomeController();
 
   return (
     <>
@@ -16,18 +16,7 @@ const Home: NextPage = () => {
         <div className={`${styles['landingHero']}`}>
         <h1>React Bootstrap NextJS Modular SCSS Boilerplate</h1>
         <h2>Lightning fast, highly optimised pages Google will love</h2>
-        <div>
-          <div className={`${styles['loginBtnWrapper']}`}>
-            {!showLoginCard &&
-              <button className={`${styles['btn']} ${styles['btn-primary']}`} onClick={fn.toggleLoginCard}>Login</button>
-            }
-          </div>
-          <div className={`${styles['loginCardWrapper']}`}>
-            {showLoginCard &&
-              <LoginCard />
-            }
-          </div>
-        </div>
+        
       </div>
         </div>
       </div>
