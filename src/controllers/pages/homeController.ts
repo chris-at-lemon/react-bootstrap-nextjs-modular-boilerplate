34c0b9ca48033ea49a297a952e8dfb78b561f607
Chris Herrmann <cherrmann@lemon-digital.com>
2022-07-16 15:02:06 +0200

    add saved searches

--- src/controllers/pages/homeController.ts
+++ src/controllers/pages/homeController.ts
@@ -4,104 +4,104 @@ import { getCity, getWeather } from '../../modules/location';
 export const HomeController = () => {
 	// Source of truth: Coordinates decide location
 	// Coordinates also decide weather props
-	const [currentCoord, setCurrentCoord] = useState({
-		lat: 37.3911379,
-		lon: -5.9938443
-	});
-	//console.log(currentCoord)
+	 const [currentCoord, setCurrentCoord] = useState({
+	 	lat: 37.3911379,
+	 	lon: -5.9938443
+	 });
+	 //console.log(currentCoord)
 
-	// City name always a result of coordinates
-	const [currentCity, setCurrentCity] = useState({
-		city: 'Berlin',
-		countryCode: 'de'
-	})
+	// // City name always a result of coordinates
+	// const [currentCity, setCurrentCity] = useState({
+	// 	city: 'Berlin',
+	// 	countryCode: 'de'
+	// })
 
 
-	interface WeatherObject {
-		temperature?: number,
-		name?: string,
-		descr?: string,
-		feels_like?: number,
-		icon?: string,
-		mainCondition?: string
-	};
+	// interface WeatherObject {
+	// 	temperature?: number,
+	// 	name?: string,
+	// 	descr?: string,
+	// 	feels_like?: number,
+	// 	icon?: string,
+	// 	mainCondition?: string
+	// };
 
-	// Weather always a result of coordinates
-	const [currentWeather, setCurrentWeather] = useState<WeatherObject>({
-		// for weather background testing
-		// temperature: 33,
-		// name: 'Berlin',
-		// descr: 'feels great',
-		// feels_like: 32,
-		// icon: '01d',
-		// mainCondition: 'Thunderstorm'
-	})
-	//console.log(currentWeather);
+	// // Weather always a result of coordinates
+	// const [currentWeather, setCurrentWeather] = useState<WeatherObject>({
+	// 	// for weather background testing
+	// 	// temperature: 33,
+	// 	// name: 'Berlin',
+	// 	// descr: 'feels great',
+	// 	// feels_like: 32,
+	// 	// icon: '01d',
+	// 	// mainCondition: 'Thunderstorm'
+	// })
+	// //console.log(currentWeather);
 
-	// Run once when app loads to set coordinates and city if geoLocation available
-	useEffect(() => {
-		const getCurrentPosition = () => navigator.geolocation.getCurrentPosition(onSuccess, onError);
-		getCurrentPosition();
+	// // Run once when app loads to set coordinates and city if geoLocation available
+	// useEffect(() => {
+	// 	const getCurrentPosition = () => navigator.geolocation.getCurrentPosition(onSuccess, onError);
+	// 	getCurrentPosition();
 
-		// set new coordinates, city and weather
-		function onSuccess(position: any) {
-			const {
-				latitude,
-				longitude
-			} = position.coords;
+	// 	// set new coordinates, city and weather
+	// 	function onSuccess(position: any) {
+	// 		const {
+	// 			latitude,
+	// 			longitude
+	// 		} = position.coords;
 
-			// set new city
-			let newCoordinates = { ...currentCoord };
-			newCoordinates = {
-				lat: latitude,
-				lon: longitude
-			}
+	// 		// set new city
+	// 		let newCoordinates = { ...currentCoord };
+	// 		newCoordinates = {
+	// 			lat: latitude,
+	// 			lon: longitude
+	// 		}
 
-			setCurrentCoord(newCoordinates);
-			fetchCity(newCoordinates.lat, newCoordinates.lon);
-			fetchWeather(newCoordinates.lat, newCoordinates.lon)
-		}
-		// Catch error
-		function onError() {
-			console.log('not allowed');
-		}
-	}, [])
+	// 		setCurrentCoord(newCoordinates);
+	// 		fetchCity(newCoordinates.lat, newCoordinates.lon);
+	// 		fetchWeather(newCoordinates.lat, newCoordinates.lon)
+	// 	}
+	// 	// Catch error
+	// 	function onError() {
+	// 		console.log('not allowed');
+	// 	}
+	// }, [])
 
-	// set new city
-	const fetchCity = async (lat: number, lon: number) => {
-		const newCityData: any = await getCity(lat, lon);
-		let newCity = { ...currentCity }
+	// // set new city
+	// const fetchCity = async (lat: number, lon: number) => {
+	// 	const newCityData: any = await getCity(lat, lon);
+	// 	let newCity = { ...currentCity }
 
-		if (newCityData.status === 200) {
-			//console.log(newCityData.data);
-			newCity = {
-				city: newCityData.data[0].name,
-				countryCode: newCityData.data[0].country
-			}
-			setCurrentCity(newCity)
-		} else {
-			console.log('error');
-		}
-	}
+	// 	if (newCityData.status === 200) {
+	// 		//console.log(newCityData.data);
+	// 		newCity = {
+	// 			city: newCityData.data[0].name,
+	// 			countryCode: newCityData.data[0].country
+	// 		}
+	// 		setCurrentCity(newCity)
+	// 	} else {
+	// 		console.log('error');
+	// 	}
+	// }
 
-	// set new weather
-	const fetchWeather = async (lat: number, lon: number) => {
-		const weatherData: any = await getWeather(lat, lon);
-		console.log(weatherData);
+	// // set new weather
+	// const fetchWeather = async (lat: number, lon: number) => {
+	// 	const weatherData: any = await getWeather(lat, lon);
+	// 	console.log(weatherData);
 
-		let newWeather = { ...currentWeather }
+	// 	let newWeather = { ...currentWeather }
 
-		newWeather = {
-			temperature: weatherData.data.main.temp,
-			name: weatherData.data.name,
-			descr: weatherData.data.weather[0].description,
-			feels_like: weatherData.data.main.feels_like,
-			icon: weatherData.data.weather[0].icon,
-			mainCondition: weatherData.data.weather[0].main
-		}
+	// 	newWeather = {
+	// 		temperature: weatherData.data.main.temp,
+	// 		name: weatherData.data.name,
+	// 		descr: weatherData.data.weather[0].description,
+	// 		feels_like: weatherData.data.main.feels_like,
+	// 		icon: weatherData.data.weather[0].icon,
+	// 		mainCondition: weatherData.data.weather[0].main
+	// 	}
 
-		setCurrentWeather(newWeather);
-	}
+	// 	setCurrentWeather(newWeather);
+	// }
 
 	// set new city and coordinates from dropdown
 	const setNewCoord = (lat: number, lon: number) => {
@@ -112,8 +112,8 @@ export const HomeController = () => {
 		}
 		setCurrentCoord(newCoord);
 		
-		fetchCity(lat, lon);
-		fetchWeather(lat, lon);
+		// fetchCity(lat, lon);
+		// fetchWeather(lat, lon);
 		
 		saveSearchHistory(newCoord);
 	}
@@ -128,9 +128,9 @@ export const HomeController = () => {
 		}
 	
 	return {
-		currentCoord,
-		currentCity,
-		currentWeather,
+		// currentCoord,
+		// currentCity,
+		// currentWeather,
 		fn: {
 			setNewCoord
 		}
