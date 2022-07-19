c7e7ae204080fd50df945c910df79c2aacf99c94
Chris Herrmann <cherrmann@lemon-digital.com>
2022-07-19 14:36:19 +0200

    update meta info

--- src/components/layout/LayoutMain.tsx
+++ src/components/layout/LayoutMain.tsx
@@ -1,46 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import Head from 'next/head';
+import React, { useEffect, useState } from "react";
+import Head from "next/head";
 
-
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Row, Col } from "reactstrap";
 
 const LayoutMain = ({ children }: any) => {
+  return (
+    <>
+      <Head>
+        <title>Rainbow Weather</title>
+        <meta name="title" content="Rainbow Weather" key="title_name" />
+        <meta name="description" content="Get current weather frm around the world" key="description_name" />
 
-	return (
-		<>
-			<Head>
-				<title>SEO and Speed test friendly boilerplates</title>
-				<meta name="title" content="SEO and Speed test friendly boilerplates" key="title_name" />
-				<meta
-					name="description"
-					content="Lightning fast, highly optimised pages Google will love"
-					key="description_name"
-				/>
+        <meta property="og:type" content="website" key="type_property" />
+        <meta property="og:site_name" content="Rainbow Weather" key="name_property" />
+        <meta property="og:title" content="Get current weather frm around the world" key="title_property" />
+        <meta property="og:description" content="Get current weather frm around the world" key="description_property" />
+        <meta property="og:keywords" content="Weather" key="keywords_property" />
+        <meta property="og:url" content="https://weather-app-omega-virid.vercel.app/" key="url" />
+        <meta name="application-name" content="Rainbow Weather" key="appName" />
+      </Head>
 
-				<meta property="og:type" content="website" key="type_property" />
-				<meta property="og:site_name" content="Lemon Digital Design" key="name_property" />
-				<meta property="og:title" content="SEO and Speed test friendly boilerplates" key="title_property" />
-				<meta
-					property="og:description"
-					content="Lightning fast, highly optimised pages Google will love"
-					key="description_property"
-				/>
-				<meta property="og:keywords" content="SEO, Google, Speed test" key="keywords_property" />
-				<meta property="og:url" content="https://www.carsvansandbikes.com" key="url" />
-				<meta
-					property="og:image"
-					itemProp="image"
-					content="https://cdn.carsvansandbikes.com/image/upload/media/documents/SharingLogo-1200x630.png"
-					key="image"
-				/>
-				<meta name="application-name" content="lemon-digital.com" key="appName" />
-			</Head>
-
-			<div className='mainWrapper'>
-				{children}
-			</div>
-		</>
-	)
-}
+      <div className="mainWrapper">{children}</div>
+    </>
+  );
+};
 
 export default LayoutMain;
